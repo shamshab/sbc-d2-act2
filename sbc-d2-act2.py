96bd74ae8b19163a30b80a0d66d672bda1390eef
shamshab <shamon.salvaloza@gmail.com>
2024-06-19 19:44:03 +0800

    act2

--- sbc-d2-act2.py
+++ sbc-d2-act2.py
@@ -1,21 +1,34 @@
-from random import choice
+from random import randint
 
-p1 = input("Fold/Unfold: ")
+p1 = (input("Fold/Unfold:"))
+c2 = randint(1,2)
+c3 = randint(1,2) 
 
-c2 = choice(["Fold", "Unfold"])
-c3 = choice(["Fold", "Unfold"])
+print("Player 1:", p1)
 
+print("Computer 2:")
+if c2 == 1:
+    print("Fold")
+else:
+    print("Unfold")
 
-print(f"Player 1: {p1}")
-print(f"Com 2: {c2}")
-print(f"Com 3: {c3}")
+print("Computer 3:")
+if c3 == 1:
+    print("Fold")
+else:
+    print("Unfold")
 
+c2_choose = "Fold" if c2 == 1 else "Unfold"
+c3_choose = "Fold" if c3 == 1 else "Unfold"
 
-if (p1 == "Fold" and c2 == "Unfold" and c3 == "Unfold") or (p1 == "Unfold" and c2 == "Fold" and c3 == "Fold"):
+if (p1 == "Fold" and c2_choose == "Unfold" and c3_choose == "Unfold") or (p1 == "Unfold" and c2_choose == "Fold" and c3_choose == "Fold"):
     print("Player 1 Win!")
-elif (p1 == "Unfold" and c2 == "Fold" and c3 == "Unfold") or (p1 == "Fold" and c2 == "Unfold" and c3 == "Fold"):
-    print("Com 2 Win!")
-elif (p1 == "Unfold" and c2 == "Unfold" and c3 == "Fold") or (p1 == "Fold" and c2 == "Fold" and c3 == "Unfold"):
-    print("Comp 3 Win!")
+
+elif (p1 == "Unfold" and c2_choose == "Fold" and c3_choose == "Unfold") or (p1 == "Fold" and c2_choose == "Unfold" and c3_choose == "Fold"):
+    print("Computer 2 Win!")
+
+elif (p1 == "Unfold" and c2_choose == "Unfold" and c3_choose == "Fold") or (p1 == "Fold" and c2_choose == "Fold" and c3_choose == "Unfold"):
+    print("Computer 3 Win!")
+
 else:
     print("No one wins!")
